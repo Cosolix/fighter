57707abc0f70bf2c4f0b8e95cf01097aaedc7fa4
Benjamin <reactiveneon@gmail.com>
2019-09-25 14:00:26 -0400

    Basic Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+    public float jumpForce = 200;
+    public int jumps = 2;
+
+    PlayerController pc;
+
+    private void Start()
+    {
+        pc = GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        float input = Input.GetAxisRaw("Horizontal") * speed;
+        Vector2 targetDisplacement = new Vector2(input, 0);
+        pc.move(targetDisplacement);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pc.jump(jumpForce);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float width = 1;
+    public float height = 0.98f;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void jump(float force) {
+        rb.AddForce(new Vector2(0, force));
+    }
+
+    public void move(Vector2 displacement) 
+    {
+        Vector2 target = new Vector2(transform.position.x + displacement.x, transform.position.y);
+        transform.Translate(displacement);
+    }
+}
